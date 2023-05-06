f5f9278f082347d2126a671bf5b50e5f7dffa078
IT21380532 <98506978+IT21380532@users.noreply.github.com>
2023-05-06 09:43:01 +0530

    Initial commit of the JobSpot app

---
+++ app/src/main/java/com/example/JobSpot/activity_removesupply.kt
@@ -0,0 +1,42 @@
+package com.example.JobSpot
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.JobSpot.databinding.ActivityRemovesupplyBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class activity_removesupply : AppCompatActivity() {
+    private lateinit var binding : ActivityRemovesupplyBinding
+    private lateinit var deleteRef : DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRemovesupplyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cancelDelete.setOnClickListener {
+            var intent = Intent(this@activity_removesupply,activity_supplieshome::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRemove.setOnClickListener {
+            var deleteSupplier = binding.edtDelete.text.toString()
+            if(deleteSupplier.isNotEmpty())
+                deleteData(deleteSupplier)
+            else
+                Toast.makeText(this,"Please enter JobID",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun deleteData(deleteSupplier: String) {
+        deleteRef = FirebaseDatabase.getInstance().getReference("Supplier")
+        deleteRef.child(deleteSupplier).removeValue().addOnSuccessListener {
+            Toast.makeText(this,"Job deleted successfully",Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener{
+            Toast.makeText(this,"Failed operation",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
